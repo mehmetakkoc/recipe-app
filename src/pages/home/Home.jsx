e24ccddf917c08c7b01c73e6afe66b99abaeddc6
makkoc86 <makkoc27@gmail.com>
2022-03-27 22:50:40 +0200

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import Box from "@mui/material/Box";
-import { Button, FormControl } from "@mui/material";
 import { useState } from "react";
 import axios from "axios";
-import Recipe from "../../components/recipe/Recipe";
+
 import { useNavigate } from "react-router-dom";
+import { FormContainer, Button, FoodInput, Select } from "./HomeStyles";
 
 const Home = () => {
   const [query, setQuery] = useState("");
@@ -15,39 +13,51 @@ const Home = () => {
     mealType[0].toLowerCase()
   );
 
-   const navigate = useNavigate();
+ 
 
   const myId = "ae0ba4fc";
   const myKey = "39548faacdda9d3da15d5f8c63746993";
   const url = ` https://api.edamam.com/search?q=${query}&app_id=${myId}&app_key=${myKey}&mealType=${selectedMealType}`;
 
-  const getRecipes = async () => {
-    const result = await axios.get(url);
-    setrecipes(result.data.hits);
-    console.log(result);
-  };
-  const submitHandle = (e) => {
+ const getData = async () => {
+   console.log(query);
+   if (query) {
+     const { data } = await axios.get(url);
+     setrecipes(data.hits);
+   } else {
+     console.log("please enter your meal");
+   }
+ };
+  const handleSubmit = (e) => {
     e.preventDefault();
-    getRecipes();
+    getData();
   };
 
   return (
     <div>
-      {/* <Recipe recipe={recipes}/> */}
-      <Box component="form" noValidate autoComplete="off">
-        <FormControl sx={{ width: "25ch" }} onSubmit={submitHandle}>
-          <OutlinedInput
-            placeholder="Please enter ingridient"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-        </FormControl>
-        <Button variant="contained" label="Submit" onClick={getRecipes}>
-          Search
-        </Button>
-        <Button variant="contained" onClick={navigate("/recipe")} >Contained</Button>
-      </Box>
-      <p  >  </p>
+      {" "}
+      <FormContainer onSubmit={handleSubmit}>
+        <FoodInput
+          type="text"
+          placeholder="search"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <Button type="submit">Search</Button>
+
+        <Select
+          name="mealTypes"
+          id="mealTypes"
+          onChange={(e) => setSelectedMealType(e.target.value)}
+        >
+          {mealType.map((item, index) => (
+            <option key={index} value={item.toLowerCase()}>
+              {item}
+            </option>
+          ))}
+        </Select>
+      </FormContainer>{" "}
+
+
     </div>
   );
 };
--- src/pages/home/HomeStyles.jsx
+++ src/pages/home/HomeStyles.jsx
@@ -1,7 +1,111 @@
 import styled from "styled-components";
+import { FlexContainer } from "../../components/globalStyles/GlobalStyles";
 
-const Div = styled.div`
-  /* ... */
+export const HeaderContainer = styled(FlexContainer)`
+  background-color: #00adb5;
+  flex-direction: column;
 `;
 
-export default Div;
+export const MainHeader = styled.h2`
+  margin-top: 1rem;
+  font-size: 3rem;
+`;
+
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  border: 2px solid white;
+  border-radius: 3px;
+  margin: 20px;
+`;
+
+export const FoodInput = styled.input`
+  height: 3rem;
+  width: 15rem;
+  border: none;
+  border-radius: 3px;
+  outline: none;
+  text-indent: 10px;
+  margin: 5px;
+  font-size: 2rem;
+`;
+
+export const Button = styled.button`
+  background-color: #e1f1dd;
+  padding: 5px;
+  outline: none;
+  height: 3rem;
+  border: none;
+  margin: 5px;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 2rem;
+  &:hover {
+    background-color: #00adb5;
+    transition: all 0.3s ease-in;
+    border: 1px solid white;
+  }
+`;
+
+export const Select = styled.select`
+  border-radius: 3px;
+  margin: 5px;
+  padding: 5px;
+  height: 3rem;
+  border: none;
+  font-size: 2rem;
+`;
+
+export const MainContainer = styled(FlexContainer)`
+  flex-direction: row;
+  background-color: #00adb5;
+`;
+
+export const RecipeCard = styled(FlexContainer)`
+  flex-direction: column;
+  height: 300px;
+  background: #e1f1dd;
+  padding: 5px;
+  border-radius: 3px;
+  width: 300px;
+  margin: 10px;
+  box-shadow: 3px 3px 10px 1px rgba(0, 0, 0, 0.3);
+  &:hover {
+    box-shadow: none;
+    transition: all 0.3s ease-in;
+  }
+`;
+
+export const RecipeImage = styled.img`
+  height: 150px;
+  border-radius: 10px;
+`;
+
+export const ButtonDetails = styled.button`
+  background-color: #00adb5;
+  padding: 5px;
+  outline: none;
+  height: 2rem;
+  border: none;
+  margin: 10px;
+  border-radius: 3px;
+  cursor: pointer;
+`;
+
+export const RecipeHeader = styled.h1`
+  font-size: 1.5rem;
+  text-align: center;
+`;
+
+export const ImgDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 50px;
+`;
+
+export const HomeImg = styled.img`
+  width: 80%;
+  max-width: 750px;
+`;
